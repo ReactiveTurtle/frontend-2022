2d96c6394417915fc506ad9c95a84210de2f211c
ReactiveTurtle <aktuganovqwerty@gmail.com>
2022-07-18 10:31:22 +0300

    commit

---
+++ ToDoList.Backend/Repositories/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using To_Do_List_Backend.Domain;
+
+namespace To_Do_List_Backend.Repositories
+{
+    public interface ITodoRepository
+    {
+        List<Todo> GetTodos();
+        Todo Get( int id );
+        int Create( Todo todo );
+        void Delete( Todo todo );
+        int Update( Todo todo );
+    }
+}
